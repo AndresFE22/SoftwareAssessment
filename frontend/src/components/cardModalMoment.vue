bb08e2c8019c0c0a41770d9dbe2c7786cfb2f216
AndrésFE22 <aarteagaramos@correo.unicordoba.edu.co>
2024-05-08 16:58:44 -0500

    update

--- frontend/src/components/cardModalMoment.vue
+++ frontend/src/components/cardModalMoment.vue
@@ -33,6 +33,7 @@
         </v-card>
         </div>
         <transition name="slide">
+          
     
         <div class="right-panel">
           <!-- <component :is="card" @change="change" /> -->
@@ -74,13 +75,23 @@
               </v-row>
               <v-row v-if="momento" style="background-color: #ececec; border-radius: 20px;">
                 <v-col cols="12">
-          <p class="titulo"></p>
-          <p class="codigo">{{ momento.curso }}</p>
+                  <br>
+                  <p class="codigo">{{ momento.curso }}</p>
           <p class="codigo-comp">{{ obtenerClaveMo(momento.PI) }}</p>
+          <br>
           <p class="descripcion"  >{{ obtenerContenidoMo(momento.PI) }}</p>
-          <p class="nombre">{{ momento.AIM }}</p>
-          <button @click="downloadPDF">Descargar PDF</button>
+          <br>
+          <br>
 
+          <div class="download">
+            <div class="pdf">
+              <p class="nombre">{{ momento.AIM }}</p>
+            </div>
+            <div class="aim">
+              <v-btn color="primary" @click="downloadPDF">Descargar PDF</v-btn>
+            </div>
+          </div>
+     
 
 
 
@@ -91,13 +102,25 @@
           </v-card-text>
         </v-card>
         </div>
-        <transition name="slide">
-    
+
         <div class="right-panelw">
-          <!-- <component :is="card" @change="change" /> -->
+          <div class="menu">
+      <v-spacer></v-spacer>
+      <v-btn @click="showInfo(1)">Indicador 1</v-btn>
+      <v-btn @click="showInfo(2)">Indicador 2</v-btn>
+      <v-btn @click="showInfo(3)">Indicador 3</v-btn>
+    </div><br>
+    <div class="text" v-if="selectedIndicator">
+      <h2>Indicador {{ key_indicador }}</h2>
+      <br>
+      <p>{{ selectedIndicator.indicador }}</p>
+      <br>
+      <h2>Evidencia</h2>
+      <br>
+      <p>{{ selectedIndicator.evidencia }}</p>
+    </div>
         </div>
-      </transition>
-    
+
         <v-btn @click="closec" icon class="absolute top-0 right-0 m-2">
                 <v-icon>mdi-close</v-icon>
               </v-btn>
@@ -137,7 +160,10 @@
         return {
           momento: null,
           dialog: true,
-          link: null
+          link: null,
+          selectedIndicator: null,
+          key_indicador: null
+
 
     
     
@@ -155,8 +181,19 @@
       methods: {
         obtenerInformacion() {
           this.momento = json.momentos.find(momento => momento.id === this.parametro)
+          this.indicadores = this.momento['indicadores']
           console.log('momento', this.momento)
+          console.log('indicadores', this.indicadores )
+
         },
+        showInfo(param) {
+          console.log('funcion indicadores')
+      this.selectedIndicator = this.indicadores[param];
+      this.key_indicador = param;
+      console.log('param', param)
+      console.log('selectedIndicador', this.selectedIndicator)
+
+    },
         obtenerClaveMo(mo) {
       return mo.split(":")[0];
     },
@@ -167,11 +204,22 @@
     closec() {
       this.$emit('closec')
     },
-    
+   
     downloadPDF() {
+      let fileID = ''
+      if (this.parametro == 'm1') {
+        fileID = '1Qr59cm0rqC-48No3kkN73hWXRqMEQBpC'
+      } else if (this.parametro == 'm2') {
+        fileID = '1_3_h8v9-3eDjBpIIvk6Y_tKti7h73k21'
+
+      } else {
+        fileID = '1SJxT0Do5oOPZg8Wh-TmISL1O_dPo2WKR'
+
+      }
       console.log("descargar pdf")
+      console.log("id", fileID)
+
       const link = document.createElement('a');
-      const fileID = '1Qr59cm0rqC-48No3kkN73hWXRqMEQBpC'
       link.href = `https://drive.google.com/uc?export=download&id=${fileID}`;
       link.download = 'AIM1.pdf';
       document.body.appendChild(link);
@@ -195,57 +243,54 @@
     </script>
     
     <style scoped>
-    .carta {
-      width: 100%; /* La carta ocupará todo el ancho disponible */
-    }
-    
-    .ficha {
-      margin: 10px; /* Espaciado entre las fichas */
-    }
-    
-    
-    .titulo {
-      font-size: 24px;
-      font-weight: bold;
-      margin-bottom: 10px;
-      text-align: center;
-      color: rgb(0, 121, 235)
-    
-    }
 
-    .embed-container {
-  position: relative;
+
+.download 
+{
+  display: flex;
   width: 100%;
-  height: 0;
-  padding-bottom: 56.25%; /* Proporción 16:9 */
+  height: auto;
 }
-.embed-container embed {
-  position: absolute;
-  top: 0;
-  left: 0;
+
+.pdf {
+  display: grid;
+  place-items: center;
   width: 100%;
-  height: 100%;
+
 }
+.aim {
+  display: grid;
+  place-items: center;
+  width: 100%;
+
+}
+    
+    
+
+
     
     .codigo {
-      font-size: 20px;
-      color: #d30000;
+      font-size: 22px;
+      color: #389fff;
       text-align: center;
-      font-weight: bold
+      font-weight: bold;
+      
     
     }
     
     .nombre {
-      font-size: 24px;
+      font-size: 38px;
       margin-top: 10px;
       text-align: center;
-      font-weight: bold
+      font-weight: bold;
+      color: #389fff;
+
     
     }
     
     .codigo-comp {
-      font-size: 16px;
-      color: #555555;
+      font-size: 23px;
+      color: #6b6b6b;
       margin-top: 5px;
       text-align: center;
       font-weight: bold
@@ -255,7 +300,7 @@
     
     .descripcion {
       font-size: 18px;
-      color: #575757;
+      color: #2b2b2b;
       margin-top: 20px;
     }
     
@@ -276,15 +321,7 @@
       height: auto;
       overflow: auto
     }
-    
-    .transition-enter-active, .transition-leave-active {
-      transition: opacity 0.5s;
-    }
-    
-    .transition-enter, .transition-leave-to {
-      opacity: 0;
-    }
-    
+
     .left-panelw {
       background-size: cover;
       padding: 20px;
@@ -297,9 +334,7 @@
     .right-panelw {
       padding: 20px;
       width: 40%;
-    
       border-radius: 20px;
-    
       height: 50vh;
     }
     
@@ -308,6 +343,38 @@
       margin-bottom: 20px;
     }
     
+    .menu {
+  display: flex;
+}
+
+.menu button {
+  margin-right: 10px;
+}
+
+.info {
+  margin-top: 20px;
+  display: grid;
+  place-items: center;
+}
+
+.info h3 {
+  margin-top: 10px;
+}
+
+.text {
+  background-color: rgb(241, 241, 241);
+  padding: 10px;
+}
+
+.text h2 {
+  color: #389fff;
+}
+
+.text p {
+  font-size: 18px;
+      color: #2b2b2b;
+}
+
     
     </style>
     
